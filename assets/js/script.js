724ecfd7298b80d506a8dc4186966aa48a61f892
TheCatWest <catwestover@outlook.com>
2021-02-27 10:13:25 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// 1. You will have X amount of time to complete the quiz.
+// 2. Correct answers earn you <span>points toward your score</span>.
+// 3. Incorrect answers <span>deduct 10 seconds<span> from your remaining time.
+// 4. Once you finish, or time runs out, you can save your score and view other high scores.
+
+// Questions
+
+/*
+What does HTML stand for?
+Hyper Text Makeup Language
+Hyper Type Markup Language
+Hyper Text Markup Language
+Hyper Test Makeup Language
+
+What does CSS stand for?
+Cross Site Scripting
+Cascading Style Sheet
+Computer Software Services
+Computing and Software Systems
+
+What tag is used to define a table or image notation?
+<code>
+<link>
+<embed>
+<caption>
+
+What tag is used to define an unordered list item?
+<li>
+<s>
+<il>
+<ul>
+
+In JavaScript, what element is used to store and manipulate text, usually in multiples?
+Recorders
+Variables
+Arrays
+Strings
+
+What is the format called that is used for storing and transporting data?
+Syntax
+JSON
+HTML
+Font
+
+What property is used to specify whether or not an element should appear as if it's on top of or above other content on the page?
+Float
+Top Level
+Jumper
+Foreground
+
+What are the CSS properties that are used to add space around sections of content?
+Break
+Cleaner
+Padding
+Spacing
+
